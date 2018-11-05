99baa585d0a1a5070e09709c03213e89a896879d
François Trahay <francois.trahay@telecom-sudparis.eu>
2018-11-05 11:09:42 +0100

    new cpu: skylake

--- src/numap.c
+++ src/numap.c
@@ -29,7 +29,7 @@ struct archi {
   const char *counting_write_event;
 };
 
-#define NB_SUPPORTED_ARCHS 14
+#define NB_SUPPORTED_ARCHS 15
 
 /* If your Intel CPU is not supported by Numap, you need to add a new architecture:
  * .id can be found by running lscpu (CPU Familly | Model <<8)
@@ -137,6 +137,18 @@ struct archi _06_4F_BROADWELL = {
 };
 
 // Processors:
+//   - Xeon Platinum 8160F (server)
+struct archi _06_55_SKYLAKE = {
+  .id = 0x06 | 0x55 << 8, // 06_85
+  .name = "Skylake micro arch",
+  .sampling_read_event = "MEM_TRANS_RETIRED:LOAD_LATENCY:ldlat=3",
+  .sampling_write_event = "MEM_UOPS_RETIRED:ALL_STORES",
+  .counting_read_event = NOT_SUPPORTED,
+  .counting_write_event = NOT_SUPPORTED
+};
+
+
+// Processors:
 //   - Xeon E5 2660 (server)
 struct archi _06_3E_IVY_BRIDGE_E = {
   .id = 0x06 | 0x3E << 8, // 06_62
@@ -215,6 +227,7 @@ static struct archi *supported_archs[NB_SUPPORTED_ARCHS] = {
   &_06_3A_IVY_BRIDGE,
   &_06_3C_HASWELL_DT,
   &_06_4F_BROADWELL,
+  &_06_55_SKYLAKE,
   &_06_3E_IVY_BRIDGE_E,
   &_06_3F_HASWELL_E,
   &_06_45_HASWELL_ULT,
