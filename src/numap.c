69b08187158bfbdaa7b60371a2c063b041b18c08
François Trahay <francois.trahay@telecom-sudparis.eu>
2022-06-02 14:44:21 +0200

    add support for memory-only NUMA domains

--- src/numap.c
+++ src/numap.c
@@ -316,9 +316,6 @@ __attribute__((constructor)) void init(void) {
         }
       }
       numa_bitmask_free(mask);
-      if (numa_node_to_cpu[node] == -1) {
-        nb_numa_nodes = -1; // to be handled properly
-      }
     }
   }
 
@@ -486,22 +483,31 @@ int __numap_counting_start(struct numap_counting_measure *measure, struct perf_e
 
   // Open the events on each NUMA node with Linux system call
   for (int node = 0; node < measure->nb_nodes; node++) {
-    measure->fd_reads[node] = perf_event_open(pe_attr_read, -1, numa_node_to_cpu[node], -1, 0);
-    if (measure->fd_reads[node] == -1) {
-      return ERROR_PERF_EVENT_OPEN;
-    }
-    measure->fd_writes[node] = perf_event_open(pe_attr_write, -1, numa_node_to_cpu[node], -1, 0);
-    if (measure->fd_writes[node] == -1) {
-      return ERROR_PERF_EVENT_OPEN;
+    if(numa_node_to_cpu[node] >= 0) {
+      measure->fd_reads[node] = perf_event_open(pe_attr_read, -1, numa_node_to_cpu[node], -1, 0);
+      if (measure->fd_reads[node] == -1) {
+	return ERROR_PERF_EVENT_OPEN;
+      }
+      measure->fd_writes[node] = perf_event_open(pe_attr_write, -1, numa_node_to_cpu[node], -1, 0);
+      if (measure->fd_writes[node] == -1) {
+	return ERROR_PERF_EVENT_OPEN;
+      }
+    } else {
+      measure->fd_reads[node] = -1;
+      measure->fd_writes[node] = -1;
     }
   }
 
   // Starts measure
   for (int node = 0; node < measure->nb_nodes; node++) {
-    ioctl(measure->fd_reads[node], PERF_EVENT_IOC_RESET, 0);
-    ioctl(measure->fd_reads[node], PERF_EVENT_IOC_ENABLE, 0);
-    ioctl(measure->fd_writes[node], PERF_EVENT_IOC_RESET, 0);
-    ioctl(measure->fd_writes[node], PERF_EVENT_IOC_ENABLE, 0);
+    if(measure->fd_reads[node] >= 0) {
+      ioctl(measure->fd_reads[node], PERF_EVENT_IOC_RESET, 0);
+      ioctl(measure->fd_reads[node], PERF_EVENT_IOC_ENABLE, 0);
+    }
+    if(measure->fd_writes[node] >= 0) {
+      ioctl(measure->fd_writes[node], PERF_EVENT_IOC_RESET, 0);
+      ioctl(measure->fd_writes[node], PERF_EVENT_IOC_ENABLE, 0);
+    }
   }
 
   return 0;
@@ -545,18 +551,27 @@ int numap_counting_stop(struct numap_counting_measure *measure) {
   }
 
   for (int node = 0; node < nb_numa_nodes; node++) {
-    ioctl(measure->fd_reads[node], PERF_EVENT_IOC_DISABLE, 0);
-    ioctl(measure->fd_writes[node], PERF_EVENT_IOC_DISABLE, 0);
-    if(read(measure->fd_reads[node], &measure->reads_count[node],
-        sizeof(long long)) == -1) {
-      return ERROR_READ;
+    if(measure->fd_reads[node] >= 0)
+      ioctl(measure->fd_reads[node], PERF_EVENT_IOC_DISABLE, 0);
+
+    if(measure->fd_writes[node] >= 0)
+      ioctl(measure->fd_writes[node], PERF_EVENT_IOC_DISABLE, 0);
+
+    if(measure->fd_reads[node] >= 0) {
+      if(read(measure->fd_reads[node], &measure->reads_count[node],
+	      sizeof(long long)) == -1) {
+	return ERROR_READ;
+      }
+      close(measure->fd_reads[node]);
     }
-    if (read(measure->fd_writes[node], &measure->writes_count[node],
-         sizeof(long long)) == -1) {
-      return ERROR_READ;
+
+    if(measure->fd_writes[node] >= 0) {
+      if (read(measure->fd_writes[node], &measure->writes_count[node],
+	       sizeof(long long)) == -1) {
+	return ERROR_READ;
+      }
+      close(measure->fd_writes[node]);
     }
-    close(measure->fd_reads[node]);
-    close(measure->fd_writes[node]);
   }
 
   return 0;
